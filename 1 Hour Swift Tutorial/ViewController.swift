cd6667f6da378e8df38d15cfd7797fb7f9d348c1
LeRoy Bainbridge <leroybainbridge@irvine.local>
2018-08-10 18:15:13 -0400

    Initial Commit

---
+++ 1 Hour Swift Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1 Hour Swift Tutorial
+//
+//  Created by Leroy Bainbridge on 8/10/18.
+//  Copyright © 2018 Leroy Bainbridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
